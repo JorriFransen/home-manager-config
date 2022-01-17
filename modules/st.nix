26707f7ba36031d6d85e390d56dd5ee8b2a97947
Jorri Fransen <jorri@jorrifransen.nl>
2022-01-17 20:24:16 +0100

    New st version.

--- modules/st.nix
+++ modules/st.nix
@@ -5,9 +5,8 @@
       src = fetchFromGitHub {
         owner = "JorriFransen";
         repo = "st";
-        rev = "6a0be17d4e6967fcabde56dbd96da872ba98154c";
-        #hash = "sha256-do2cpdclphzDs3SaO5c6J/5Iu4wQXDfDNQ2o9naMf3u=";
-        hash = "sha256-pzZ6znQO0g0RPmZLgWCSg/FgVZUu+Cj0WrXHqRgIw8o=";
+        rev = "f3f0dba60c6254825415c8921c355e2e56489633";
+        hash = "sha256-Q/66KfT+8ZXebXjYXQiEPRRMmXQwZMRFXYrShPlLsxE=";
       };
       buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
     }))
